15f99173359c9c2f344763e36b1c1714982d08e4
kevin.RAJENDRAM <kevin.rajendram@digitalseeder.com>
2022-05-11 13:44:08 +0200

    refactor code and change relation to ManytoOne between users and roles

--- api/src/main/java/com/sirioitalia/api/model/User.java
+++ api/src/main/java/com/sirioitalia/api/model/User.java
@@ -1,12 +1,7 @@
 package com.sirioitalia.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 import javax.persistence.*;
 import java.sql.Date;
 import java.time.LocalDate;
@@ -14,24 +9,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.sql.Date;
+import javax.validation.constraints.Email;
 
 
-
-@Entity // This tells Hibernate to make a table out of this class
+@Builder
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-@Table(name = "USER")
-public class User implements Serializable {
+@Table(name = "users")
+public class User {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Getter
     @Column(name = "name")
     private String name;
 
+    @Getter
+    @Setter
     @Column(name = "email")
     private String email;
 
@@ -59,43 +57,7 @@ public class User implements Serializable {
     @Column(name = "createdAt")
     private Date createdAt;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles = new HashSet<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "\"roleId\"", nullable = false)
+    private Role roleId;
 }
